92eda5e4d8143a7d82f1630ffbf78803a590a27b
enger <pvl.zolotov@gmail.com>
2020-01-21 00:51:39 +0300

    GlideSliderView created

---
+++ library/src/main/java/com/daimajia/slider/library/SliderTypes/GlideSliderView.java
@@ -0,0 +1,29 @@
+package com.daimajia.slider.library.SliderTypes;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+
+public class GlideSliderView extends DefaultSliderView {
+
+    public GlideSliderView(Context context) {
+        super(context);
+    }
+
+    protected void bindEventAndShow(final View v, ImageView targetImageView) {
+        View progressBar = v.findViewById(com.daimajia.slider.library.R.id.loading_bar);
+        if (progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
+
+        Glide.with(getContext())
+                .load(getUrl())
+                .centerCrop()
+                .transition(withCrossFade())
+                .into(targetImageView);
+    }
+}
